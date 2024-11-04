94e1b9ec83049177e8e727b31fb956713b0c1794
Roopan Vishnu <153484476+darqsparkle@users.noreply.github.com>
2024-11-04 14:01:21 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="App"> {/* Add the class name "App" to this div */}
       <h1>
         SOMETHING COOKING
       </h1>
