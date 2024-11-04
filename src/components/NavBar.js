d502768aafc721959d3ca1b59b2b674e0f60844a
Roopan Vishnu <153484476+darqsparkle@users.noreply.github.com>
2024-11-04 17:41:09 +0530

    navbar styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,29 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
+import './NavBar.css'; // Import the CSS file for styling
 
 const NavBar = () => {
     return (
         <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about-us">About Us</Link>
-            </li>
-            <li>
-              <Link to="/expertise">Expertise</Link>
-            </li>
-            <li>
-              <Link to="/our-products">Our Products</Link>
-            </li>
-          </ul>
+            <ul className="navbar">
+                <li className="brand">
+                    <Link to="/">Ryuworks</Link>
+                </li>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about-us">About Us</Link>
+                </li>
+                <li>
+                    <Link to="/expertise">Expertise</Link>
+                </li>
+                <li>
+                    <Link to="/our-products">Our Products</Link>
+                </li>
+            </ul>
         </nav>
-      );
+    );
 }
 
-export default NavBar
+export default NavBar;
