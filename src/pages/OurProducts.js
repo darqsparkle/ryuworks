87e09a107280c4b730617c8bb8a2d43d79b28df8
Roopan Vishnu <153484476+darqsparkle@users.noreply.github.com>
2024-11-04 17:34:52 +0530

    navbar error fixing commit

--- src/pages/OurProducts.js
+++ src/pages/OurProducts.js
@@ -3,7 +3,7 @@ import React from 'react';
 function OurProducts() {
   return(
     <div>
-        <h2>About Us</h2>
+        <h2>Our Products</h2>
         <h3>We are COOKING SOMETHING</h3>
     </div>
   );
